18c1f08828151210f0b81d4205ae6c659f9270ab
Aner Zakobar <aner@zakobar.com>
2025-05-25 09:44:28 +0300

    Qutebrowser DRM overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,3 +1,5 @@
 {inputs, ...}: {
   addpkgs = final: _prev: import ../pkgs {pkgs = final;};
+  qutebrowserdrm = final: _prev: {
+    qutebrowser = _prev.qutebrowser.override { enableWideVine = true; }; };
 }
