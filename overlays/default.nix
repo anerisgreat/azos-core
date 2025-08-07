a35f8d7a65ad2dd41f81517a4fb3d6454a3f6221
Aner Zakobar <aner@zakobar.com>
2025-08-07 22:54:45 +0300

    Useless

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: {
-  addpkgs = final: _prev: import ../pkgs {pkgs = final;};
+  addpkgs = final: _prev: import ../pkgs {pkgs = final; };
   qutebrowserdrm = final: _prev: {
     qutebrowser = _prev.qutebrowser.override { enableWideVine = true; }; };
 }
