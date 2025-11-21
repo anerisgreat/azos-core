1d6c38a27f5ce2caf1067d26251d3b5eedd2dae9
Aner Zakobar <aner@zakobar.com>
2025-11-21 14:19:27 +0200

    Temp trying

--- modules/home-manager/suites/base/emacs/default.nix
+++ modules/home-manager/suites/base/emacs/default.nix
@@ -11,29 +11,29 @@
   emacspkgs = config.azos.emacs.emacspkg.pkgs;
   localPkgName = "azos-emacs-base";
   # This is because of problems with package versions, remove when you can
-  emacsPackageOverrideOverlay = self: super:
-    {
-        emacsPackages = super.emacsPackages // {
-            org = super.org.override {
-                elpaBuild = args: super.elpaBuild (args // {
-                    version = "9.27.37";
-                    src = super.fetchurl {
-                        url = "https://elpa.gnu.org/packages/org-9.7.37.tar";
-                        sha256 = "HXugakRVPZ9gWvFHVDlgbrhGrcf1DQT5iJ28wHnIedk";
-                    };
-                });
-            };
-            exwm = super.exwm.override {
-                elpaBuild = args: super.elpaBuild (args // {
-                    version = "0.34";
-                    src = super.fetchurl {
-                        url = "https://elpa.gnu.org/packages/exwm-0.34.tar";
-                        sha256 = "33R9rEiis2HbZefibaLdVUcGv7Fn9HLEEcYuw1K04sI";
-                    };
-                });
-            };
-        };
-    };
+  # emacsPackageOverrideOverlay = self: super:
+  #   {
+  #       emacsPackages = super.emacsPackages // {
+  #           org = super.org.override {
+  #               elpaBuild = args: super.elpaBuild (args // {
+  #                   version = "9.27.37";
+  #                   src = super.fetchurl {
+  #                       url = "https://elpa.gnu.org/packages/org-9.7.37.tar";
+  #                       sha256 = "HXugakRVPZ9gWvFHVDlgbrhGrcf1DQT5iJ28wHnIedk";
+  #                   };
+  #               });
+  #           };
+  #           exwm = super.exwm.override {
+  #               elpaBuild = args: super.elpaBuild (args // {
+  #                   version = "0.34";
+  #                   src = super.fetchurl {
+  #                       url = "https://elpa.gnu.org/packages/exwm-0.34.tar";
+  #                       sha256 = "33R9rEiis2HbZefibaLdVUcGv7Fn9HLEEcYuw1K04sI";
+  #                   };
+  #               });
+  #           };
+  #       };
+  #   };
 
 in {
   options.azos.emacs.enable = azos-utils.mkFeatureEnableOption {
@@ -71,9 +71,8 @@ in {
       # package = (pkgs.emacsPackagesFor config.azos.emacs.emacspkg).emacsWithPackages (
       #   config.azos.emacs.pkgs
       # );
-      package = ((
+      package = (
         pkgs.emacsPackagesFor config.azos.emacs.emacspkg
-      ).overrideScope emacsPackageOverrideOverlay
       ).emacsWithPackages (
         config.azos.emacs.pkgs
       );
