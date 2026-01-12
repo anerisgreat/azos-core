63f8c40ce9fb7df84d66373bed57fe83611d2ad2
Aner Zakobar <aner@zakobar.com>
2026-01-12 22:27:24 +0200

    flycheck-irony

--- pkgs/azos-emacs-dev.nix
+++ pkgs/azos-emacs-dev.nix
@@ -8,7 +8,6 @@ orgTrivialBuild {
   version = "0.1.6";
   src = ./elisp/azos-emacs-dev.org;
   packageRequires = with epkgs; [
-    flycheck
     cmake-mode
     yaml
     yaml-mode
@@ -19,8 +18,10 @@ orgTrivialBuild {
     flycheck-rust
     ein
     ob-async
+    flycheck
+    irony
+    flycheck-irony
     pkgs.azos-emacs-base
     pkgs.azos-emacs-editor
-    irony
   ];
 }
