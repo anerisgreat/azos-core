6969769ac8a218d1feeb686775e4ddbffef4bd9b
Aner Zakobar <aner@zakobar.com>
2026-02-26 12:35:37 +0200

    Integrate LLM stuff into azos core

--- pkgs/azos-emacs-editor.nix
+++ pkgs/azos-emacs-editor.nix
@@ -2,7 +2,17 @@
   orgTrivialBuild,
   epkgs,
   pkgs,
-}:
+}: let
+  ghgptel = (epkgs.callPackage epkgs.trivialBuild {
+      pname = "gptel";
+      version = "0.9.9.3";
+      src = pkgs.fetchFromGitHub {
+        owner = "karthink";
+        repo = "gptel";
+        rev = "273c0f93958c1ffa85e396717b504903eda36bce"; # Use a specific commit hash for reproducibility
+        sha256 = "sha256-gVgdFLi6RGUCD3ZXzOIo5XpTNmP/9xMAO5nyWu1zVlM=";
+      };
+    })
 orgTrivialBuild {
   pname = "azos-emacs-editor";
   version = "0.1.6";
@@ -16,5 +26,32 @@ orgTrivialBuild {
     org-present
     pdf-tools
     pandoc-mode
+    ghgptel
+    (epkgs.callPackage epkgs.trivialBuild {
+      pname = "gptel-autocomplete";
+      version = "2025-06-18";
+      src = pkgs.fetchFromGitHub {
+        owner = "JDNdeveloper";
+        repo = "gptel-autocomplete";
+        rev = "8ace326a6e7b8a3a4df7a6e80272b472e7fbd167"; # Use a specific commit hash for reproducibility
+        sha256 = "sha256-gVgdFLi6RGUCD3ZXzOIo5XpTNmP/9lMAO5nyWu1zVlM=";
+      };
+      buildInputs = with epkgs; [
+        ghgptel
+      ];
+    })
+    (epkgs.callPackage epkgs.trivialBuild {
+      pname = "gptel-agent";
+      version = "0.0.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "karthink";
+        repo = "gptel-agent";
+        rev = "8ba9056da2341468192e6417d47cb50e26636e97"; # Use a specific commit hash for reproducibility
+        sha256 = "sha256-M2J/K3UHoAbDWQjYPD8ZdL6uHBggvPh+ZvJ+xnbXJuo=";
+      };
+      buildInputs = with epkgs; [
+        ghgptel
+      ];
+    })
   ];
 }
