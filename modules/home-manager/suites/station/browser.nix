fb0e28e251e74828666537237fa525701f997254
Aner Zakobar <aner@zakobar.com>
2026-03-25 22:29:28 +0200

    Fix xdg.mimeApps option name

--- modules/home-manager/suites/station/browser.nix
+++ modules/home-manager/suites/station/browser.nix
@@ -20,10 +20,13 @@ in {
       chromium
     ];
 
-    xdg.mime.defaultApplications = {
-      "x-scheme-handler/http" = "qutebrowser.desktop";
-      "x-scheme-handler/https" = "qutebrowser.desktop";
-      "text/html" = "qutebrowser.desktop";
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/http" = "qutebrowser.desktop";
+        "x-scheme-handler/https" = "qutebrowser.desktop";
+        "text/html" = "qutebrowser.desktop";
+      };
     };
   };
 }
